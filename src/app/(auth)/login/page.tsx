278e11213aeaaaa1379e6d3a683f71d953a6e8bb
Tatjana Ribic <tb@linkmedia.com>
2024-10-10 16:37:08 +0400

    init login form structure

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,19 @@
 function Login() {
-  return <h1>Login</h1>
+  return (
+    <div>
+      <form>
+        <div className='input-row'>
+          <label htmlFor='email'>Email address</label>
+          <input name='email' type='email' />
+        </div>
+        <div className='input-row'>
+          <label htmlFor='password'>Password</label>
+          <input name='password' type='password' />
+        </div>
+        <button type='submit'>Log in</button>
+      </form>
+    </div>
+  )
 }
 
 export default Login
