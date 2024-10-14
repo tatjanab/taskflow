f229c268bd78f61d49f619667029588d05ccd701
Tatjana Ribic <tb@linkmedia.com>
2024-10-14 15:15:16 +0400

    added react hook form for the login form, separated in in a component

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,53 +1,7 @@
-import { Input } from '@chakra-ui/react'
-import { Command } from 'react-feather'
+import LoginForm from '@/components/LoginForm'
 
 function Login() {
-  return (
-    <div className='flex min-h-screen w-full flex-col items-center overflow-hidden bg-zinc-100 sm:pt-10'>
-      <div className='w-full bg-white p-10 shadow-sm sm:w-1/2 lg:w-1/3'>
-        <div className='flex flex-row items-center text-blue-900'>
-          <Command height={30} />{' '}
-          <span className='ml-1 font-bold'>TaskFlow</span>
-        </div>
-
-        <h1 className='mb-2 mt-4 text-xl font-bold'>Welcome back!</h1>
-        <p className='text-xs'>Login to your account to continue.</p>
-        <form className='mt-6'>
-          <div className='input-row mb-4 flex flex-col'>
-            <label htmlFor='email' className='mb-1 text-xs'>
-              Email address
-            </label>
-            <Input
-              name='email'
-              type='email'
-              height='30px'
-              className='w-full p-2 text-xs'
-            />
-          </div>
-          <div className='input-row mb-4 flex flex-col'>
-            <label htmlFor='password' className='mb-1 text-xs'>
-              Password
-            </label>
-            <Input
-              name='password'
-              type='password'
-              height='30px'
-              className='w-full p-2 text-xs'
-            />
-          </div>
-          <button
-            type='submit'
-            className='btn-main w-full rounded-md bg-blue-600 px-5 py-2 text-xs uppercase text-white'
-          >
-            Log in
-          </button>
-          <p className='mt-4 text-xs'>
-            Don&apos;t have an account? Sign up here.
-          </p>
-        </form>
-      </div>
-    </div>
-  )
+  return <LoginForm />
 }
 
 export default Login
---
+++ src/app/components/LoginForm.tsx
@@ -0,0 +1,107 @@
+'use client'
+import { Input } from '@chakra-ui/react'
+import { Command } from 'react-feather'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+type FormFields = {
+  email: string
+  password: string
+}
+
+function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    setError,
+  } = useForm<FormFields>()
+
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      console.log(data)
+    } catch (error) {
+      setError('root', {
+        message: 'An error has occurred. Please try again',
+      })
+    }
+  }
+
+  return (
+    <div className='flex min-h-screen w-full flex-col items-center overflow-hidden bg-zinc-100 sm:pt-10'>
+      <div className='w-full bg-white p-10 shadow-sm sm:w-1/2 lg:w-1/3'>
+        <div className='flex flex-row items-center text-blue-900'>
+          <Command height={30} />{' '}
+          <span className='ml-1 font-bold'>TaskFlow</span>
+        </div>
+
+        <h1 className='mb-2 mt-4 text-xl font-bold'>Welcome back!</h1>
+        <p className='text-xs'>Login to your account to continue.</p>
+        <form className='mt-6' onSubmit={handleSubmit(onSubmit)}>
+          <div className='input-row mb-4 flex flex-col'>
+            <label htmlFor='email' className='mb-1 text-xs'>
+              Email address
+            </label>
+            <Input
+              {...register('email', {
+                required: 'Email address is required',
+                validate: (value) => {
+                  if (!value.includes('@')) {
+                    return 'Email must include @'
+                  }
+
+                  return true
+                },
+              })}
+              name='email'
+              type='text'
+              height='30px'
+              className='w-full p-2 text-xs'
+            />
+            {errors.email && (
+              <div className='text-red-500 text-xs'>{errors.email.message}</div>
+            )}
+          </div>
+          <div className='input-row mb-4 flex flex-col'>
+            <label htmlFor='password' className='mb-1 text-xs'>
+              Password
+            </label>
+            <Input
+              {...register('password', {
+                required: 'Password is required',
+                minLength: {
+                  value: 8,
+                  message: 'Password must have at least  8 characters',
+                },
+              })}
+              name='password'
+              type='password'
+              height='30px'
+              className='w-full p-2 text-xs'
+            />
+            {errors.password && (
+              <div className='text-red-500 text-xs'>
+                {errors.password.message}
+              </div>
+            )}
+          </div>
+          <button
+            type='submit'
+            disabled={isSubmitting}
+            className='btn-main w-full rounded-md bg-blue-600 px-5 py-2 text-xs uppercase text-white'
+          >
+            {isSubmitting ? 'Loading...' : 'Login'}
+          </button>
+          {errors.root && (
+            <div className='text-red-500 text-xs'>{errors.root.message}</div>
+          )}
+          <p className='mt-4 text-xs'>
+            Don&apos;t have an account? Sign up here.
+          </p>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default LoginForm
