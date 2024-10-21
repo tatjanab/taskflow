e3bf6044ce0958fc990218e48fe43cc38cd0a795
Tatjana Ribic <tb@linkmedia.com>
2024-10-21 17:21:30 +0400

    ui updates

--- src/app/components/NewTaskBtn.tsx
+++ src/app/components/NewTaskBtn.tsx
@@ -13,9 +13,6 @@ import {
 } from '@chakra-ui/react'
 
 import { useForm, SubmitHandler } from 'react-hook-form'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-
 // import taskSchema from '@/models/zod_schema'
 import { TaskItem } from '@/models/types'
 
@@ -55,61 +52,76 @@ function NewTaskBtn() {
           <ModalBody>
             <form onSubmit={handleSubmit(handleAddTask)}>
               <div>
-                <div className='flex flex-col mb-3'>
-                  <label className='text-xs'>Summary</label>
+                <div className='flex flex-col mb-3 w-1/2'>
+                  <label className='text-xs mb-1'>Summary</label>
                   <input
                     type='text'
                     {...register('summary')}
-                    className='text-xs'
+                    className='text-xs border-gray-400 border p-1'
                   />
                   {errors.summary && <p>{errors.summary.message}</p>}
                 </div>
-                <div className='flex flex-col mb-3'>
-                  <label className='text-xs'>Type</label>
-                  <select className='text-xs' {...register('type')}>
-                    <option value='Feature'>Feature</option>
-                    <option value='Improvement'>Improvement</option>
-                    <option value='Task'>Task</option>
-                    <option value='Bug'>Bug</option>
-                  </select>
-                  {errors.type && <p>{errors.type.message}</p>}
+                <div className='flex flex-row gap-4'>
+                  <div className='flex flex-col mb-3 w-1/2'>
+                    <label className='text-xs mb-1'>Type</label>
+                    <select
+                      className='text-xs border-gray-400 border p-1'
+                      {...register('type')}
+                    >
+                      <option value='Feature'>Feature</option>
+                      <option value='Improvement'>Improvement</option>
+                      <option value='Task'>Task</option>
+                      <option value='Bug'>Bug</option>
+                    </select>
+                    {errors.type && <p>{errors.type.message}</p>}
+                  </div>
+                  <div className='flex flex-col mb-3 w-1/2'>
+                    <label className='text-xs mb-1'>Priority</label>
+                    <select
+                      className='text-xs border-gray-400 border p-1'
+                      {...register('details.priority')}
+                    >
+                      <option value='High'>High</option>
+                      <option value='Medium'>Medium</option>
+                      <option value='Low'>Low</option>
+                    </select>
+                    {errors.details?.priority && (
+                      <p>{errors.details?.priority.message}</p>
+                    )}
+                  </div>
                 </div>
-                <div className='flex flex-col mb-3'>
-                  <label className='text-xs'>Status</label>
-                  <select className='text-xs' {...register('status')}>
+
+                <div className='flex flex-col mb-3 w-1/2'>
+                  <label className='text-xs mb-1'>Status</label>
+                  <select
+                    className='text-xs border-gray-400 border p-1'
+                    {...register('status')}
+                  >
                     <option value='Open'>Open</option>
                     <option value='In progress'>In progress</option>
                     <option value='Done'>Done</option>
                   </select>
                   {errors.status && <p>{errors.status.message}</p>}
                 </div>
-                <div className='flex flex-col mb-3'>
-                  <label className='text-xs'>Assignee</label>
+                <div className='flex flex-col mb-3 w-1/2'>
+                  <label className='text-xs mb-1'>Assignee</label>
                   <input
                     type='text'
                     {...register('details.assignee')}
-                    className='text-xs'
+                    className='text-xs border-gray-400 border p-1'
                   />
                   {errors.details?.assignee && (
                     <p>{errors.details?.assignee.message}</p>
-                  )}
-                </div>
-                <div className='flex flex-col mb-3'>
-                  <label className='text-xs'>Priority</label>
-                  <select className='text-xs' {...register('details.priority')}>
-                    <option value='High'>High</option>
-                    <option value='Medium'>Medium</option>
-                    <option value='Low'>Low</option>
-                  </select>
-                  {errors.details?.priority && (
-                    <p>{errors.details?.priority.message}</p>
                   )}
                 </div>
               </div>
 
               <div className='flex flex-col mb-3'>
                 <label className='text-xs'>Description</label>
-                <textarea {...register('description')}></textarea>
+                <textarea
+                  {...register('description')}
+                  className='border-gray-400 border p-1'
+                ></textarea>
                 {errors.description && <p>{errors.description.message}</p>}
               </div>
               <Button
