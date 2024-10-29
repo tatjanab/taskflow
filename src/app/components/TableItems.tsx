8e51cb3015b6ca7b2f269d5e88dfd7011248bb49
Tatjana Ribic <tb@linkmedia.com>
2024-10-29 16:32:37 +0400

    fix loader

--- src/app/components/TableItems.tsx
+++ src/app/components/TableItems.tsx
@@ -13,37 +13,39 @@ function TableItems({ taskList, isLoading }: TaskLists) {
   const taskListSorted = taskList.sort((a, b) => a._id - b._id)
   return (
     <>
-      {taskListSorted.map((item) => {
-        const formattedCreationDate = item.details.creationDate
-          ? format(new Date(item.details.creationDate), 'yyyy-MM-dd')
-          : 'N/A'
+      {isLoading
+        ? Array(10)
+            .fill(0)
+            .map((_, index) => <TableItemLoader key={index} />)
+        : taskListSorted.map((item) => {
+            const formattedCreationDate = item.details.creationDate
+              ? format(new Date(item.details.creationDate), 'yyyy-MM-dd')
+              : 'N/A'
 
-        return isLoading ? (
-          <TableItemLoader key={Math.random()} />
-        ) : (
-          <Tr
-            key={item._id}
-            className='hover:bg-slate-100 hover:cursor-pointer'
-          >
-            <Td p='8px' width='40px'>
-              {item._id}
-            </Td>
-            <Td p='8px'>{item.summary}</Td>
-            <Td p='8px'>
-              <span className='rounded-sm bg-green-100 p-1 font-medium text-green-700'>
-                {item.status}
-              </span>
-            </Td>
-            <Td p='8px'>{item.details.assignee}</Td>
-            <Td p='8px'>{item.details.priority}</Td>
-            <Td p='8px'>
-              <span className='rounded-sm bg-slate-100 p-1 font-medium text-slate-500'>
-                {formattedCreationDate}
-              </span>
-            </Td>
-          </Tr>
-        )
-      })}
+            return (
+              <Tr
+                key={item._id}
+                className='hover:bg-slate-100 hover:cursor-pointer'
+              >
+                <Td p='8px' width='40px'>
+                  {item._id}
+                </Td>
+                <Td p='8px'>{item.summary}</Td>
+                <Td p='8px'>
+                  <span className='rounded-sm bg-green-100 p-1 font-medium text-green-700'>
+                    {item.status}
+                  </span>
+                </Td>
+                <Td p='8px'>{item.details.assignee}</Td>
+                <Td p='8px'>{item.details.priority}</Td>
+                <Td p='8px'>
+                  <span className='rounded-sm bg-slate-100 p-1 font-medium text-slate-500'>
+                    {formattedCreationDate}
+                  </span>
+                </Td>
+              </Tr>
+            )
+          })}
     </>
   )
 }
