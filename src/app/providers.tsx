3db2b0ad53378e0d1ffdfc1783e2edd47487d206
Tatjana Ribic <tb@linkmedia.com>
2025-02-11 17:14:45 +0400

    sidebar updated to use shadcnui

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,6 +2,8 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import AppSidebar from '@/components/sidebar/Sidebar'
 
 const queryClient = new QueryClient()
 
@@ -9,11 +11,15 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        {children}
-        <ReactQueryDevtools
-          initialIsOpen={false}
-          buttonPosition='bottom-right'
-        />
+        <SidebarProvider>
+          <AppSidebar />
+          <main className='flex-1'>{children}</main>
+
+          <ReactQueryDevtools
+            initialIsOpen={false}
+            buttonPosition='bottom-right'
+          />
+        </SidebarProvider>
       </QueryClientProvider>
     </>
   )
